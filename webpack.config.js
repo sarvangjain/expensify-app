6bbff621ca3bb3ae29eec2b7777120b1d67284c1
sarvangjain <18ucs062@lnmiit.ac.in>
2020-08-13 16:09:24 +0530

    Setup Dev-Dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
         entry: './src/app.js',
         mode: 'development',
         output: {
-            path: path.join(__dirname,'public'),
+            path: path.join(__dirname,'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -45,7 +45,8 @@ module.exports = (env) => {
             contentBase: path.join(__dirname,'public'),
             disableHostCheck: true,
             overlay: true,
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     }
 };
