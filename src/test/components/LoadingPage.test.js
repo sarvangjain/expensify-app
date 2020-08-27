8a6e14928f352d189dafb464d05f1671151dc3d2
sarvangjain <18ucs062@lnmiit.ac.in>
2020-08-27 12:28:52 +0530

    Final Deployment

--- src/test/components/LoadingPage.test.js
+++ src/test/components/LoadingPage.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import LoadingPage from '../../components/LoadingPage';
 
 test('should correctly render LoadingPage', () => {
   const wrapper = shallow(<LoadingPage />);
-  expect(wrapper).toMatchSnapshot();
+  expect(toJSON(wrapper)).toMatchSnapshot();
 });
